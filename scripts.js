963c709495a5ac150c8dd5c4fac524b106c44096
Annika McDermott-Hinman <annikamh406@gmail.com>
2023-09-11 13:45:47 -0400

    fix hamburger?

--- scripts.js
+++ scripts.js
@@ -1,8 +1,3 @@
-// Set the initial state to hidden
-document.addEventListener("DOMContentLoaded", (event) => {
-    const menu = document.querySelector('.mobile-nav');
-    menu.style.display = 'none';
-  });
   
   function toggleMenu() {
     const menu = document.querySelector('.mobile-nav');
