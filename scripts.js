2d4a28c3c4326cf6d6e4472e03babca4457db428
Annika McDermott-Hinman <annikamh406@gmail.com>
2023-09-11 11:54:33 -0400

    hide menu

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
-function toggleMenu() {
+// Set the initial state to hidden
+document.addEventListener("DOMContentLoaded", (event) => {
+    const menu = document.querySelector('.mobile-nav');
+    menu.style.display = 'none';
+  });
+  
+  function toggleMenu() {
     const menu = document.querySelector('.mobile-nav');
     menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
