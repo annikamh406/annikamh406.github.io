c2961e7d770bb376a3f85788ad475f3a65913a39
Annika McDermott-Hinman <annikamh406@gmail.com>
2023-09-11 15:54:50 -0400

    hamburger menu fix

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-  
-  function toggleMenu() {
-    const menu = document.querySelector('.mobile-nav');
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-}
+document.addEventListener('DOMContentLoaded', (event) => {
+    function toggleMenu() {
+        const menu = document.querySelector('.mobile-nav');
+        menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+    }
+})
